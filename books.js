429dea8f39f1330fcd01b48d622d7081528f57ec
shubhamagrawal19398 <49454815+shubhamagrawal19398@users.noreply.github.com>
2020-02-02 12:09:25 +0530

    Add files via upload

---
+++ books.js
@@ -0,0 +1,338 @@
+function initializeCount()
+{
+	this.count=0;
+	this.book=[];
+	
+}
+
+function decrement1()
+{    var quantity1=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }   
+	 else{
+	 	count++;
+    document.getElementById("book1").innerHTML="Quantity = " + (--quantity1);
+   book.push("Biography Of Rahul Dravid");
+    alert("Book Issued succefully");
+    }
+ }
+
+ function decrement2()
+{    var quantity2=50;
+    if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }  
+	 else{
+	 	count++;
+    document.getElementById("book2").innerHTML="Quantity = " + (--quantity2);
+    book.push("Brief History Of Time");
+    alert("Book Issued succefully");
+    }
+	    
+ }
+
+ function decrement3()
+{    var quantity3=50;
+	 
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }  else{
+	 	count++;
+    document.getElementById("book3").innerHTML="Quantity = " + (--quantity3);
+    book.push("A Century is not Enough");
+    alert("Book Issued succefully");
+	 }  
+ }
+
+ function decrement4()
+{    var quantity4=50;
+	   
+	if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }   else{
+	 	count++;
+    document.getElementById("book4").innerHTML="Quantity = " + (--quantity4);
+    book.push("Steve Jobs");
+    alert("Book Issued succefully");
+	}
+ }
+
+ function decrement5()
+{    var quantity5=50;
+ 
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book5").innerHTML="Quantity = " + (--quantity5);
+    book.push("Beginning Android Programming with Android Studio, 4ed");
+    alert("Book Issued succefully");
+      }
+	 
+ }
+
+ function decrement6()
+{    var quantity6=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book6").innerHTML="Quantity = " + (--quantity6);
+    book.push("Data Structures and Algorithms Made Easy");   
+    alert("Book Issued succefully");
+	  }  
+ }
+
+ function decrement7()
+{    var quantity7=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book7").innerHTML="Quantity = " + (--quantity7);
+    book.push("DEVELOP SELF-CONFIDENCE, IMPROVE PUBLIC SPEAKING");
+    alert("Book Issued succefully");
+
+	 }   
+ }
+
+ function decrement8()
+{    var quantity8=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book8").innerHTML="Quantity = " + (--quantity8);
+    book.push("Elon Musk");
+    alert("Book Issued succefully");
+	  }  
+ }
+
+ function decrement9()
+{    var quantity9=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book9").innerHTML="Quantity = " + (--quantity9);
+    book.push("Engineering Mechanics");
+    alert("Book Issued succefully");
+	  }  
+ }
+
+ function decrement10()
+{    var quantity10=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book10").innerHTML="Quantity = " + (--quantity10);
+    book.push("How to Win Friends and Influence People");
+    alert("Book Issued succefully");
+	  }  
+ }
+
+ function decrement11()
+{    var quantity11=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book11").innerHTML="Quantity = " + (--quantity11);
+    book.push("KOTLIN FOR ANDROID DEVELOPERS");
+    alert("Book Issued succefully");
+	  }  
+ }
+
+ function decrement12()
+{    var quantity12=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book12").innerHTML="Quantity = " + (--quantity12);
+    book.push("Man's Search for Meaning");
+    alert("Book Issued succefully");
+	  }  
+ }
+
+ function decrement13()
+{    var quantity13=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book13").innerHTML="Quantity = " + (--quantity13);
+    book.push("Playing It My Way: My Autobiography");
+    alert("Book Issued succefully");
+    }
+ }
+
+ function decrement14()
+{    var quantity14=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book14").innerHTML="Quantity = " + (--quantity14);
+    book.push("Shoe Dog : A Memoir By the Creator of Nike");
+    alert("Book Issued succefully");
+	  }  
+ }
+
+ function decrement15()
+{    var quantity15=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book15").innerHTML="Quantity = " + (--quantity15);
+    book.push("Prophet of Modern India: A Biography of Swami Vivekananda");
+    alert("Book Issued succefully");
+	  }  
+ }
+
+ function decrement16()
+{    var quantity16=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book16").innerHTML="Quantity = " + (--quantity16);
+    book.push("Think & Grow Rich");
+    alert("Book Issued succefully");
+	  }  
+ }
+
+ function decrement17()
+{    var quantity17=50;
+	
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book17").innerHTML="Quantity = " + (--quantity17);
+    book.push("Wings of Fire: An Autobiography");
+    alert("Book Issued succefully");
+	  }  
+ }
+
+ function decrement18()
+{    var quantity18=50;
+	 
+	 if(count>=3)
+	 {
+	 	alert("you can Issue a maximum of 3 books at a time ");
+	 }    
+	 else
+	 {
+	 	count++;
+    document.getElementById("book18").innerHTML="Quantity = " + (--quantity18);
+    book.push("You Can Win");
+    alert("Book Issued succefully");
+	  }  
+ }
+
+
+ function saveDetails()
+ {
+ 	this.a=document.getElementById("name").value;
+ 	this.b=document.getElementById("regestration").value;
+ 	this.c=document.getElementById("mobile").value;
+    window.location = 'https://www.facebook.com/';
+ 	alert("Name :" + a +" regestration N0 : " + b +"mobile no : "+ c);
+ 	
+ }
+
+function call()
+{
+	
+	this.name1 = prompt("Enter your name :  ");
+	this.reg = prompt("Enter your regestration number : ");
+	document.getElementById("name").innerHTML=name1;
+	document.getElementById("reg").innerHTML=reg;
+
+}
+
+function issue()
+{   
+                  var date = new Date();
+                   var hour = date.getHours();
+                   var min = date.getMinutes();
+                   var month = date.getMonth()+1
+                   var day = date.getDate();
+                   var year = date.getFullYear();
+                   
+                   
+	document.write("<p><b>BOOK ISSUE DETAILS</b></p>");
+    document.write("<b>======================</b><br/><br/>") ;   
+	document.write("<b>Name :</b> " + name1+"<br/>");
+	document.write("<b>Regestration :</b> " + reg + "<br>" );
+	document.write("<b>Issue Date : </b>"+month+"/"+day+"/"+year+"<b>||</b>"+hour+":"+min+"<br/>");
+	document.write("<b>======================</b><br>");
+	document.write("<u><b>Issued books</b></u>" + "<br><br>");
+	for(i=0;i<count;i++)
+	{    
+
+		document.write(i+1 + ")" + book[i] + "<br>" );
+	}
+
+
+}
+ 
---
+++ journals.js
@@ -0,0 +1,19 @@
+function call()
+{
+	
+	this.count=0;
+}
+
+function issue()
+{  
+	if(count<3)
+	{
+		count++;
+	alert("Journal added to your Account successfully");
+     }
+     else
+     {
+     	alert("you can issue a maximum of 3 journal at a time");
+     }
+
+}
